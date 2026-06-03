ade6805cc10763aea9f031fffda6d83499b113c9
Agboola Usman Olayinka <agboolausman58@gmail.com>
2026-06-03 23:14:41 +0100

    r

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
+    nitro: true, // 👈 add this
     server: { 
       entry: "server",
-      preset: "vercel", // Add this line to target Vercel
+      preset: "vercel",
     },
     serverFns: {
       disableCsrfMiddlewareWarning: true,
