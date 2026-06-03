9de71271cfa394175b2325e3577b35baa0fe1fe6
Agboola Usman Olayinka <agboolausman58@gmail.com>
2026-06-03 23:23:17 +0100

    w

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  nitro: {
+    preset: "vercel",
+  },
   tanstackStart: {
-    server: { 
+    server: {
       entry: "server",
-      preset: "vercel", // This is mandatory for Vercel
     },
     serverFns: {
       disableCsrfMiddlewareWarning: true,
